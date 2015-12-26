b3d5b9754eaab3e15d59b7932e2e03a3d5776fff
Серега <@Desktop>
2015-12-26 18:15:21 +0800

    add enum

---
+++ tictactoe/common/BoardItem.java
@@ -0,0 +1,28 @@
+
+package tictactoe.common;
+
+/**
+ *
+ * @author Серега
+ */
+public enum BoardItem 
+{
+    	UNDEFINED(""),
+	X("X"),
+	O("0"),
+	X_WIN_FIELD("X"),
+	O_WIN_FIELD("0");
+
+	private String name;
+
+	private BoardItem(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
+    
+    
+
