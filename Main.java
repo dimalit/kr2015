b3c4e9e3a9c913a6339515652ebdc98c84109b43
Серега <@Desktop>
2016-01-05 22:00:59 +0200

    fix

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.lang.reflect.InvocationTargetException;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UnsupportedLookAndFeelException;
 /**
  *
@@ -18,15 +21,33 @@ public class Main
         
 		EventQueue.invokeAndWait(new Runnable() {
 			public void run() {	
-				GameFrame wnd = new GameFrame();
-				wnd.setTitle("Tic-Tac-Toe");
-				wnd.setResizable(false);
-				wnd.setVisible(true);
+		        GameFrame gameframe = new GameFrame();
+                                
+                         gameframe.setDefaultCloseOperation(GameFrame.DO_NOTHING_ON_CLOSE);
+                         gameframe.addWindowListener(new WindowAdapter() {
+             @Override
+            public void windowClosing(WindowEvent e) {
+                
+                Object options [] = {"Yes","No"};
+                                
+                int answerExit =  JOptionPane.showOptionDialog(gameframe, "Are you sure?", "Exit", 
+                          JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+                    
+                  if (answerExit == JOptionPane.YES_OPTION)
+                  {
+                      gameframe.dispose();
+                  }
+                 
+            }
+        });
+				gameframe.setTitle("Tic-Tac-Toe");
+				gameframe.setResizable(false);
+				gameframe.setVisible(true);
                                 Toolkit kit = Toolkit.getDefaultToolkit();
 		                Dimension screen = kit.getScreenSize();
 		                int w = screen.width;
 		                int h = screen.height;
-                                wnd.setLocation(w / 4, h / 4);
+                                gameframe.setLocation(w / 4, h / 4);
 			}
 		});
 
