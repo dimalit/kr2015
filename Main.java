ef2a0d79a45f45c1cc57ef96025698048bed5ab1
Серега <@Desktop>
2016-01-22 13:18:57 +0200

    fix

--- Main.java
+++ Main.java
@@ -5,11 +5,7 @@
  */
 public class Main 
 {
-
-    
-    public static void main(String[] args){
-         
+    public static void main(String[] args){        
         ThreadChoosePlayer f2 = new ThreadChoosePlayer();
-
-	}
+   }
 }
